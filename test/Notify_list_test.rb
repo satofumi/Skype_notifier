34c2722cdaa25ea4f485008b40b0a3e7c2e52d49
Satofumi KAMIMURA <satofumi@twinstar.(none)>
2010-10-19 06:14:32 +0900

    added test file

---
+++ test/Notify_list_test.rb
@@ -0,0 +1,27 @@
+#!/usr/bin/env ruby
+# -*- coding: utf-8 -*-
+
+# Notify_list.rb のテストクラス
+# Satofumi KAMIMURA
+# $Id$
+
+require 'test/unit'
+require '../Notify_list'
+
+
+class Notify_list_test < Test::Unit::TestCase
+
+  def setup
+    # !!!
+  end
+
+
+  def test_xxx
+    # !!!
+  end
+
+
+  def test_yyy
+    # !!!
+  end
+end
