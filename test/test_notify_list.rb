544b967f7ec097127cab6b6deff72df9b8286619
Satofumi KAMIMURA <satofumi@twinstar.(none)>
2010-10-19 18:31:14 +0900

    adjusted rdoc setting

--- test/test_notify_list.rb
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env ruby
-
-require '../Notify_list'
-notify_list = Notify_list.new '../notify_messages.txt'
-
-message, wait_second = notify_list.next_message_and_wait_second()
-p message, wait_second
